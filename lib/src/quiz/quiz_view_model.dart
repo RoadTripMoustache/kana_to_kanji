c32c92adf3018a8fd7238c6877e20fbb23e8a777
apomalyn <chretienxavier42@gmail.com>
2023-07-04 19:09:01 -0400

    Retrieve the kana list

--- lib/src/quiz/quiz_view_model.dart
+++ lib/src/quiz/quiz_view_model.dart
@@ -1,15 +1,21 @@
 import 'package:kana_to_kanji/src/core/models/group.dart';
+import 'package:kana_to_kanji/src/core/models/kana.dart';
+import 'package:kana_to_kanji/src/core/repositories/kana_repository.dart';
+import 'package:kana_to_kanji/src/locator.dart';
 import 'package:stacked/stacked.dart';
 
 class QuizViewModel extends FutureViewModel {
   final List<Group> groups;
 
+  final List<Kana> kana = [];
+
+  final KanaRepository _kanaRepository = locator<KanaRepository>();
+
   QuizViewModel(this.groups);
 
   @override
-  Future futureToRun() {
-    // TODO: implement futureToRun
-    // throw UnimplementedError();
-    return Future.delayed(Duration(seconds: 1));
+  Future futureToRun() async {
+    kana.addAll(await _kanaRepository
+        .getByGroupIds(groups.map((e) => e.id).toList(growable: false)));
   }
 }
