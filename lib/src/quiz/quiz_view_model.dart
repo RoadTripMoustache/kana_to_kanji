ffc3b1d8d17df4da78df9202235c413a85663716
apomalyn <chretienxavier42@gmail.com>
2023-07-04 20:19:39 -0400

    Initialize the question list

--- lib/src/quiz/quiz_view_model.dart
+++ lib/src/quiz/quiz_view_model.dart
@@ -1,21 +1,67 @@
 import 'package:kana_to_kanji/src/core/models/group.dart';
-import 'package:kana_to_kanji/src/core/models/kana.dart';
 import 'package:kana_to_kanji/src/core/repositories/kana_repository.dart';
 import 'package:kana_to_kanji/src/locator.dart';
+import 'package:kana_to_kanji/src/quiz/constants/question_types.dart';
+import 'package:kana_to_kanji/src/quiz/models/question.dart';
 import 'package:stacked/stacked.dart';
 
 class QuizViewModel extends FutureViewModel {
+  final KanaRepository _kanaRepository = locator<KanaRepository>();
+
   final List<Group> groups;
 
-  final List<Kana> kana = [];
+  final List<Question> _questions = [];
 
-  final KanaRepository _kanaRepository = locator<KanaRepository>();
+  int get quizLength => _questions.length;
+
+  int _currentQuestionIndex = 0;
+
+  int get questionNumber => _currentQuestionIndex + 1;
+
+  Question get current => _questions[_currentQuestionIndex];
 
   QuizViewModel(this.groups);
 
   @override
   Future futureToRun() async {
-    kana.addAll(await _kanaRepository
-        .getByGroupIds(groups.map((e) => e.id).toList(growable: false)));
+    final kana = await _kanaRepository
+        .getByGroupIds(groups.map((e) => e.id).toList(growable: false));
+
+    _questions.addAll(kana.map((element) =>
+        Question(question: element, type: QuestionTypes.toRomanji)));
+    _questions.shuffle();
+  }
+
+  void skipQuestion() {
+    final skipped = _questions.removeAt(_currentQuestionIndex);
+    _questions.add(skipped);
+    notifyListeners();
+  }
+
+  void validateAnswer(String answer) {
+    final question = _questions[_currentQuestionIndex];
+    String expectedAnswer;
+
+    switch(question.type) {
+      case QuestionTypes.toJapanese:
+        expectedAnswer = question.question.kana;
+        break;
+      case QuestionTypes.toRomanji:
+        expectedAnswer = question.question.romanji;
+        break;
+      case QuestionTypes.translate:
+        throw UnimplementedError();
+    }
+
+    if(answer != expectedAnswer) {
+      question.remainingAttempt -= 1;
+    } else {
+      _currentQuestionIndex++;
+
+      if(_currentQuestionIndex == _questions.length) {
+        // TODO Trigger quiz end
+      }
+    }
+    notifyListeners();
   }
 }
