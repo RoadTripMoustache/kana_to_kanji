d8883d1c8469e13a942f30249a492c79b60aef88
apomalyn <chretienxavier42@gmail.com>
2023-08-01 22:17:59 -0400

    style: Remove unused import

--- test/src/quiz/widgets/animated_dot_test.dart
+++ test/src/quiz/widgets/animated_dot_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kana_to_kanji/src/quiz/widgets/animated_dot.dart';
