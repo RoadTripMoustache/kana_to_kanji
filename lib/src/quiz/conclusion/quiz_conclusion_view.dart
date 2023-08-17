8117f69cef6524bbe736cdddf4aecd46d11ef435
apomalyn <chretienxavier42@gmail.com>
2023-08-16 22:51:58 -0400

    feat: Hide the to review section if there is nothing to review

--- lib/src/quiz/conclusion/quiz_conclusion_view.dart
+++ lib/src/quiz/conclusion/quiz_conclusion_view.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kana_to_kanji/src/core/widgets/app_scaffold.dart';
 import 'package:kana_to_kanji/src/quiz/conclusion/quiz_conclusion_view_model.dart';
 import 'package:kana_to_kanji/src/quiz/conclusion/widgets/arc_progress_indicator.dart';
-import 'package:kana_to_kanji/src/quiz/conclusion/widgets/question_review_tile.dart';
 import 'package:kana_to_kanji/src/quiz/conclusion/widgets/to_review_section.dart';
 import 'package:kana_to_kanji/src/quiz/models/question.dart';
 import 'package:stacked/stacked.dart';
 
 class QuizConclusionView extends StatelessWidget {
   static const routeName = "/quiz/conclusion";
+
+  static const _kArcProgressIndicatorSize = 200.0;
 
   final List<Question> questions;
 
@@ -21,6 +21,9 @@ class QuizConclusionView extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
     final textTheme = Theme.of(context).textTheme;
+
+    final maxTopPaddingIndicator =
+        (MediaQuery.of(context).size.height) / 2 - _kArcProgressIndicatorSize;
 
     return ViewModelBuilder<QuizConclusionViewModel>.reactive(
         viewModelBuilder: () => QuizConclusionViewModel(questions),
@@ -36,23 +39,28 @@ class QuizConclusionView extends StatelessWidget {
                 ),
                 title: Text(l10n.quiz_conclusion)),
             body: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Center(
                   child: Padding(
-                    padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                    padding: EdgeInsets.only(
+                        top: viewModel.wrongAnswers.isNotEmpty
+                            ? 20.0
+                            : maxTopPaddingIndicator,
+                        bottom: 20.0),
                     child: ArcProgressIndicator(
                         value: viewModel.percent,
-                        radius: 200,
+                        radius: _kArcProgressIndicatorSize,
                         alternativeText: l10n.quiz_length(
                             viewModel.rightAnswers.length,
                             viewModel.questions.length)),
                   ),
                 ),
-                Expanded(
-                    child: ToReviewSection(
-                        questionsToReview: viewModel.wrongAnswers)),
+                if (viewModel.wrongAnswers.isNotEmpty)
+                  Expanded(
+                      child: ToReviewSection(
+                          questionsToReview: viewModel.wrongAnswers)),
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
                   child: Row(
