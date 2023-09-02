95cb206d32554849359b550ae82852d21a3a6aae
apomalyn <chretienxavier42@gmail.com>
2023-09-02 17:38:03 -0400

    feat: Create AppConfig and main for beta

--- lib/main_beta.dart
+++ lib/main_beta.dart
@@ -3,11 +3,10 @@ import 'package:kana_to_kanji/src/app.dart';
 import 'package:kana_to_kanji/src/core/repositories/settings_repository.dart';
 import 'package:kana_to_kanji/src/core/widgets/app_config.dart';
 import 'package:kana_to_kanji/src/locator.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
   // Initialize the application
   await KanaToKanjiApp.initializeApp();
 
-  runApp(AppConfig(environment: Environment.prod, child: KanaToKanjiApp()));
+  runApp(AppConfig(environment: Environment.beta, child: KanaToKanjiApp()));
 }
