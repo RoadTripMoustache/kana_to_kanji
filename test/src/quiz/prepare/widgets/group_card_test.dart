9a8e769f12ba4611f0337c1f97721b7c6630141d
Maxime Guilbert <mx.guilbert@gmail.com>
2023-09-03 11:50:46 -0400

    fix: Unit tests

--- test/src/quiz/prepare/widgets/group_card_test.dart
+++ test/src/quiz/prepare/widgets/group_card_test.dart
@@ -10,7 +10,7 @@ import '../../../../helpers.dart';
 void main() {
   group("GroupCard", () {
     final Group groupSample =
-        Group(0, Alphabets.katakana, "Group name", KanaTypes.main, "");
+        Group(0, Alphabets.katakana, "Group name", KanaTypes.main, "Group name");
     final Group localizedGroupSample = Group(0, Alphabets.katakana,
         "Group name", KanaTypes.main, "Localized group name");
 
