bfae29af9aff27afb1d3c6c3356f1c30bc21dd70
apomalyn <chretienxavier42@gmail.com>
2023-09-05 23:52:32 -0400

    chore: Add Crashlytics to Android and iOS

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:kana_to_kanji/firebase_options.dart';
 import 'package:kana_to_kanji/src/core/constants/app_theme.dart';
 import 'package:kana_to_kanji/src/core/repositories/settings_repository.dart';
 import 'package:kana_to_kanji/src/locator.dart';
@@ -15,6 +19,17 @@ class KanaToKanjiApp extends StatelessWidget {
   static Future initializeApp() async {
     setupLocator();
     WidgetsFlutterBinding.ensureInitialized();
+
+    // Firebase initialization
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
 
     locator.allReadySync();
 
