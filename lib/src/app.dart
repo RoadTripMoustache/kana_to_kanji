62e24c8c9c86604a818527f77d9d26f9865f6e20
Maxime Guilbert <mx.guilbert@gmail.com>
2023-09-09 20:49:26 -0400

    fix: Lint

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
