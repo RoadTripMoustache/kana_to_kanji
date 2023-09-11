09c305b25acb9124439a020f9440e8b7844385b9
Maxime Guilbert <mx.guilbert@gmail.com>
2023-09-11 14:08:52 -0400

    fix: Simplify settings view code

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -67,8 +67,7 @@ class SettingsView extends StatelessWidget {
                               .map<DropdownMenuItem<Locale>>((Locale locale) {
                             return DropdownMenuItem<Locale>(
                               value: locale,
-                              child: Text(l10n.language(
-                                  locale.languageCode, locale.languageCode)),
+                              child: Text(l10n.language(locale.languageCode)),
                             );
                           }).toList(),
                         ),
