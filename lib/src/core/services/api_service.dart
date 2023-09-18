710c80c7448069b255a5c85658aaa7f9d3670c65
apomalyn <chretienxavier42@gmail.com>
2023-09-17 23:33:34 -0400

    feat(config): Create AppConfiguration and refactor ApiService to use it.

--- lib/src/core/services/api_service.dart
+++ lib/src/core/services/api_service.dart
@@ -1,8 +1,9 @@
 import 'package:http/http.dart' as http;
+import 'package:kana_to_kanji/src/core/constants/app_configuration.dart';
 
 class ApiService {
   Future<http.Response> get(String path) {
     return http.get(Uri.parse(
-        'http://10.0.2.2:8080$path')); // TODO : Move into a configuration
+        '${AppConfiguration.apiUrl}$path'));
   }
 }
