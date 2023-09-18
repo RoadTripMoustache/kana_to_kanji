84cf64fba81e117eac42481dc450c9fa110795fd
apomalyn <apomalyn@users.noreply.github.com>
2023-09-18 04:00:40 +0000

    [BOT] Applying format.

--- lib/src/core/services/api_service.dart
+++ lib/src/core/services/api_service.dart
@@ -3,7 +3,6 @@ import 'package:kana_to_kanji/src/core/constants/app_configuration.dart';
 
 class ApiService {
   Future<http.Response> get(String path) {
-    return http.get(Uri.parse(
-        '${AppConfiguration.apiUrl}$path'));
+    return http.get(Uri.parse('${AppConfiguration.apiUrl}$path'));
   }
 }
