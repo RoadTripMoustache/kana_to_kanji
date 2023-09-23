390c7d9580a276d1fb7a9c6f06e20472c4a25f76
apomalyn <chretienxavier42@gmail.com>
2023-09-23 14:09:06 -0400

    chore: Remove unused import

--- lib/src/feedback/widgets/feedback_form.dart
+++ lib/src/feedback/widgets/feedback_form.dart
@@ -1,9 +1,7 @@
 import 'dart:typed_data';
 
-import 'package:feedback/feedback.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:go_router/go_router.dart';
 import 'package:kana_to_kanji/src/feedback/constants/feedback_form_fields.dart';
 import 'package:kana_to_kanji/src/feedback/constants/feedback_type.dart';
 
