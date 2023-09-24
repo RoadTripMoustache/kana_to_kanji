04e8fc3b32aa2946cfd17b04b910030db53f74f1
apomalyn <chretienxavier42@gmail.com>
2023-09-24 12:57:03 -0400

    chore: Remove color extension as it's not used.

--- lib/src/core/utils/extensions.dart
+++ lib/src/core/utils/extensions.dart
@@ -1,23 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:kana_to_kanji/src/core/constants/alphabets.dart';
 
 extension AlphabetsExtension on Alphabets {
   bool isKana() {
     return this == Alphabets.hiragana || this == Alphabets.katakana;
   }
-}
-
-extension ColorExtension on Color {
-  MaterialColor toMaterialColor() => MaterialColor(value, {
-        50: withOpacity(.1),
-        100: withOpacity(.2),
-        200: withOpacity(.3),
-        300: withOpacity(.4),
-        400: withOpacity(.5),
-        500: withOpacity(.6),
-        600: withOpacity(.7),
-        700: withOpacity(.8),
-        800: withOpacity(.9),
-        900: withOpacity(1),
-      });
 }
