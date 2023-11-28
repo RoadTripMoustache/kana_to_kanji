9b4a068148e564091b42885796edc2f1c1f6146f
Maxime G <36586573+Adaendra@users.noreply.github.com>
2023-11-28 16:13:32 -0500

    Update lib/src/glossary/widgets/vocabulary_list.dart
    Co-authored-by: Xavier Chrétien <chretienxavier42@gmail.com>

--- lib/src/glossary/widgets/vocabulary_list.dart
+++ lib/src/glossary/widgets/vocabulary_list.dart
@@ -4,7 +4,7 @@ import 'package:kana_to_kanji/src/core/widgets/app_scaffold.dart';
 import 'package:kana_to_kanji/src/glossary/widgets/vocabulary_list_tile.dart';
 
 class VocabularyList extends StatelessWidget {
-  final List<Vocabulary> dataList;
+  final List<Vocabulary> items;
 
   const VocabularyList({super.key, required this.dataList});
 
