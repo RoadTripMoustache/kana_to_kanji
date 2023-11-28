9dc6964e7c8e4d96a588d0e9c6a883a8a340a28a
Maxime G <36586573+Adaendra@users.noreply.github.com>
2023-11-28 16:13:21 -0500

    Update lib/src/glossary/widgets/kana_list.dart
    Co-authored-by: Xavier Chrétien <chretienxavier42@gmail.com>

--- lib/src/glossary/widgets/kana_list.dart
+++ lib/src/glossary/widgets/kana_list.dart
@@ -4,7 +4,7 @@ import 'package:kana_to_kanji/src/core/widgets/app_scaffold.dart';
 import 'package:kana_to_kanji/src/glossary/widgets/kana_list_tile.dart';
 
 class KanaList extends StatelessWidget {
-  final List<Kana> dataList;
+  final List<Kana> items;
 
   const KanaList({super.key, required this.dataList});
 
