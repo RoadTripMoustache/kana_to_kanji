17c178e6e123d5126c242bb12dbf726995fb3648
Maxime Guilbert <mx.guilbert@gmail.com>
2023-12-16 14:18:53 -0500

    #96 - Setup Filter by

--- lib/src/glossary/glossary_view_model.dart
+++ lib/src/glossary/glossary_view_model.dart
@@ -1,3 +1,5 @@
+import 'package:kana_to_kanji/src/core/constants/jlpt_level.dart';
+import 'package:kana_to_kanji/src/core/constants/knowledge_level.dart';
 import 'package:kana_to_kanji/src/core/models/kana.dart';
 import 'package:kana_to_kanji/src/core/models/kanji.dart';
 import 'package:kana_to_kanji/src/core/models/vocabulary.dart';
@@ -17,6 +19,9 @@ class GlossaryViewModel extends FutureViewModel {
   final List<Kana> _katakanaList = [];
   final List<Kanji> _kanjiList = [];
   final List<Vocabulary> _vocabularyList = [];
+  final List<JLPTLevel> _selectedJlptLevel = [];
+  final List<KnowledgeLevel> _selectedKnowledgeLevel = [];
+  String _currentSearch = "";
 
   List<Kana> get hiraganaList => _hiraganaList;
 
@@ -25,6 +30,8 @@ class GlossaryViewModel extends FutureViewModel {
   List<Kanji> get kanjiList => _kanjiList;
 
   List<Vocabulary> get vocabularyList => _vocabularyList;
+  List<JLPTLevel> get selectedJlptLevel => _selectedJlptLevel;
+  List<KnowledgeLevel> get selectedKnowledgeLevel => _selectedKnowledgeLevel;
 
   GlossaryViewModel();
 
@@ -43,57 +50,24 @@ class GlossaryViewModel extends FutureViewModel {
   }
 
   void searchGlossary(String searchText) async {
-    RegExp alphabeticalRegex = RegExp(r'([a-zA-Z])$');
-    if (searchText == "") {
-      _displayAll();
-    } else if (alphabeticalRegex.hasMatch(searchText)) {
-      _searchLatin(searchText);
-    } else {
-      _searchJapanese(searchText);
-    }
+    _currentSearch = searchText;
+    _updateGlossaryList();
     notifyListeners();
   }
 
-  void _searchLatin(String searchText) async {
-    final result = await Future.wait([
-      _kanaRepository.searchHiraganaRomaji(searchText),
-      _kanaRepository.searchKatakanaRomaji(searchText)
-    ]);
-
-    _hiraganaList.clear();
-    _hiraganaList.addAll(result[0]);
-    _katakanaList.clear();
-    _katakanaList.addAll(result[1]);
-    _kanjiList.clear();
-    _kanjiList.addAll(_kanjiRepository.searchKanjiRomaji(searchText));
-    _vocabularyList.clear();
-    _vocabularyList
-        .addAll(_vocabularyRepository.searchVocabularyRomaji(searchText));
+  void filterGlossary() async {
+    await _updateGlossaryList();
+    notifyListeners();
   }
 
-  void _searchJapanese(String searchText) async {
+  Future _updateGlossaryList() async {
     final result = await Future.wait([
-      _kanaRepository.searchHiraganaKana(searchText),
-      _kanaRepository.searchKatakanaKana(searchText)
-    ]);
-
-    _hiraganaList.clear();
-    _hiraganaList.addAll(result[0]);
-    _katakanaList.clear();
-    _katakanaList.addAll(result[1]);
-    _kanjiList.clear();
-    _kanjiList.addAll(_kanjiRepository.searchKanjiJapanese(searchText));
-    _vocabularyList.clear();
-    _vocabularyList
-        .addAll(_vocabularyRepository.searchVocabularyJapanese(searchText));
-  }
-
-  void _displayAll() async {
-    final result = await Future.wait([
-      _kanaRepository.getHiragana(),
-      _kanaRepository.getKatakana(),
-      _kanjiRepository.getAll(),
-      _vocabularyRepository.getAll()
+      _kanaRepository.searchHiragana(_currentSearch, _selectedKnowledgeLevel),
+      _kanaRepository.searchKatakana(_currentSearch, _selectedKnowledgeLevel),
+      _kanjiRepository.searchKanji(
+          _currentSearch, _selectedKnowledgeLevel, _selectedJlptLevel),
+      _vocabularyRepository.searchVocabulary(
+          _currentSearch, _selectedKnowledgeLevel, _selectedJlptLevel)
     ]);
 
     _hiraganaList.clear();
---
+++ lib/src/glossary/widgets/filter_by.dart
@@ -0,0 +1,269 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:kana_to_kanji/src/core/constants/jlpt_level.dart';
+import 'package:kana_to_kanji/src/core/constants/knowledge_level.dart';
+
+class FilterBy extends StatefulWidget {
+  final void Function() filterGlossary;
+  final List<JLPTLevel> selectedJlptLevel;
+  final List<KnowledgeLevel> selectedKnowledgeLevel;
+
+  const FilterBy({
+    super.key,
+    required this.filterGlossary,
+    required this.selectedJlptLevel,
+    required this.selectedKnowledgeLevel,
+  });
+
+  @override
+  State<FilterBy> createState() => _FilterBy();
+}
+
+class _FilterBy extends State<FilterBy> {
+  late List<JLPTLevel> _selectedJlptLevel;
+  late List<KnowledgeLevel> _selectedKnowledgeLevel;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedJlptLevel = widget.selectedJlptLevel;
+    _selectedKnowledgeLevel = widget.selectedKnowledgeLevel;
+  }
+
+  void _toggleJLPTLevel(JLPTLevel jlptLevel) {
+    if (_selectedJlptLevel.contains(jlptLevel)) {
+      _selectedJlptLevel.remove(jlptLevel);
+    } else {
+      _selectedJlptLevel.add(jlptLevel);
+    }
+    setState(() {
+      _selectedJlptLevel = _selectedJlptLevel;
+    });
+  }
+
+  void _toggleKnowledgeLevel(KnowledgeLevel knowledgeLevel) {
+    if (_selectedKnowledgeLevel.contains(knowledgeLevel)) {
+      _selectedKnowledgeLevel.remove(knowledgeLevel);
+    } else {
+      _selectedKnowledgeLevel.add(knowledgeLevel);
+    }
+    setState(() {
+      _selectedKnowledgeLevel = _selectedKnowledgeLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final AppLocalizations l10n = AppLocalizations.of(context);
+
+    const titlePadding = EdgeInsets.only(left: 20, top: 8, right: 20);
+    const checkboxPadding = EdgeInsets.symmetric(horizontal: 40);
+
+    const styleTitle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.of(context).pop();
+            widget.filterGlossary();
+          },
+        ),
+        title: Text(l10n.glossary_filter_by_title),
+      ),
+      body: Column(
+        children: [
+          Row(children: [
+            Padding(
+              padding: titlePadding,
+              child: Text(
+                l10n.glossary_filter_by_jlpt_level_title,
+                textAlign: TextAlign.left,
+                style: styleTitle,
+              ),
+            ),
+          ]),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child: Text(l10n
+                      .glossary_filter_by_jlpt_level(JLPTLevel.level1.value)),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value: _selectedJlptLevel.contains(JLPTLevel.level1),
+                    onChanged: (_) => {_toggleJLPTLevel(JLPTLevel.level1)}),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child: Text(l10n
+                      .glossary_filter_by_jlpt_level(JLPTLevel.level2.value)),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value: _selectedJlptLevel.contains(JLPTLevel.level2),
+                    onChanged: (_) => {_toggleJLPTLevel(JLPTLevel.level2)}),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child: Text(l10n
+                      .glossary_filter_by_jlpt_level(JLPTLevel.level3.value)),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value: _selectedJlptLevel.contains(JLPTLevel.level3),
+                    onChanged: (_) => {_toggleJLPTLevel(JLPTLevel.level3)}),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child: Text(l10n
+                      .glossary_filter_by_jlpt_level(JLPTLevel.level4.value)),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value: _selectedJlptLevel.contains(JLPTLevel.level4),
+                    onChanged: (_) => {_toggleJLPTLevel(JLPTLevel.level4)}),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child: Text(l10n
+                      .glossary_filter_by_jlpt_level(JLPTLevel.level5.value)),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value: _selectedJlptLevel.contains(JLPTLevel.level5),
+                    onChanged: (_) => {_toggleJLPTLevel(JLPTLevel.level5)}),
+              ),
+            ],
+          ),
+          Row(children: [
+            Padding(
+              padding: titlePadding,
+              child: Text(
+                l10n.glossary_filter_by_knowledge_level_title,
+                textAlign: TextAlign.left,
+                style: styleTitle,
+              ),
+            ),
+          ]),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child: Text(l10n.glossary_filter_by_knowledge_level_learned),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value: _selectedKnowledgeLevel
+                        .contains(KnowledgeLevel.learned),
+                    onChanged: (_) =>
+                        {_toggleKnowledgeLevel(KnowledgeLevel.learned)}),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child:
+                      Text(l10n.glossary_filter_by_knowledge_level_practicing),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value: _selectedKnowledgeLevel
+                        .contains(KnowledgeLevel.practicing),
+                    onChanged: (_) =>
+                        {_toggleKnowledgeLevel(KnowledgeLevel.practicing)}),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child: Text(l10n.glossary_filter_by_knowledge_level_seen),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value:
+                        _selectedKnowledgeLevel.contains(KnowledgeLevel.seen),
+                    onChanged: (_) =>
+                        {_toggleKnowledgeLevel(KnowledgeLevel.seen)}),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding: checkboxPadding,
+                  child:
+                      Text(l10n.glossary_filter_by_knowledge_level_never_seen),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Checkbox(
+                    value: _selectedKnowledgeLevel
+                        .contains(KnowledgeLevel.neverSeen),
+                    onChanged: (_) =>
+                        {_toggleKnowledgeLevel(KnowledgeLevel.neverSeen)}),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
