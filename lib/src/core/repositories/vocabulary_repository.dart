6c9dacaf377deec2a9a180693535c722b0f896ba
apomalyn <chretienxavier42@gmail.com>
2023-12-21 19:07:48 +0100

    test: Add tests for the VocabularyRepository
    refs: #100

--- lib/src/core/repositories/vocabulary_repository.dart
+++ lib/src/core/repositories/vocabulary_repository.dart
@@ -1,17 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:kana_to_kanji/src/core/models/vocabulary.dart';
 import 'package:kana_to_kanji/src/core/services/vocabulary_service.dart';
 
 class VocabularyRepository {
-  final VocabularyService _vocabularyService = VocabularyService();
-  final List<Vocabulary> _vocabulary = [];
+  late final VocabularyService _vocabularyService;
+  @visibleForTesting
+  final List<Vocabulary> vocabularies = [];
 
+  /// [vocabularyService] should only be specified for testing purpose
+  VocabularyRepository({VocabularyService? vocabularyService}) {
+    _vocabularyService = vocabularyService ?? VocabularyService();
+  }
+
+  /// Retrieve all the vocabulary
   List<Vocabulary> getAll() {
-    if (_vocabulary.isNotEmpty) {
-      return _vocabulary;
+    if (vocabularies.isNotEmpty) {
+      return vocabularies;
     }
-    var vocabulary = _vocabularyService.getAll();
-    _vocabulary.addAll(vocabulary);
 
-    return vocabulary;
+    vocabularies.addAll(_vocabularyService.getAll());
+
+    return vocabularies;
   }
 }
