eb6fdefc80f31d6e125542a4790719a541b23513
Maxime Guilbert <mx.guilbert@gmail.com>
2024-01-21 09:04:12 -0500

    #98 - Setup Search by

--- lib/src/core/repositories/kana_repository.dart
+++ lib/src/core/repositories/kana_repository.dart
@@ -33,10 +33,52 @@ class KanaRepository {
         .toList();
   }
 
+  Future<List<Kana>> searchHiraganaRomaji(String searchTxt) async {
+    await loadKana();
+    if (searchTxt.length > 3) {
+      return Future(() => List.empty());
+    }
+    return _kana
+        .where((element) =>
+            Alphabets.hiragana == element.alphabet &&
+            element.romaji.contains(searchTxt))
+        .toList();
+  }
+
+  Future<List<Kana>> searchHiraganaKana(String searchTxt) async {
+    await loadKana();
+    return _kana
+        .where((element) =>
+            Alphabets.hiragana == element.alphabet &&
+            element.kana.contains(searchTxt))
+        .toList();
+  }
+
   Future<List<Kana>> getKatakana() async {
     await loadKana();
     return _kana
         .where((element) => Alphabets.katakana == element.alphabet)
+        .toList();
+  }
+
+  Future<List<Kana>> searchKatakanaRomaji(String searchTxt) async {
+    await loadKana();
+    if (searchTxt.length > 3) {
+      return Future(() => List.empty());
+    }
+    return _kana
+        .where((element) =>
+            Alphabets.katakana == element.alphabet &&
+            element.romaji.contains(searchTxt))
+        .toList();
+  }
+
+  Future<List<Kana>> searchKatakanaKana(String searchTxt) async {
+    await loadKana();
+    return _kana
+        .where((element) =>
+            Alphabets.katakana == element.alphabet &&
+            element.kana.contains(searchTxt))
         .toList();
   }
 }
