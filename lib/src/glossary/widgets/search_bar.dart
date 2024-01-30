4e6d80351eb68aa09cad5d7b1676456db4e7fc59
Maxime Guilbert <mx.guilbert@gmail.com>
2024-01-29 22:13:25 -0500

    fix: Tapping clear search doesn't clear it

--- lib/src/glossary/widgets/search_bar.dart
+++ lib/src/glossary/widgets/search_bar.dart
@@ -126,6 +126,7 @@ class _GlossarySearchBarState extends State<GlossarySearchBarContent> {
     _focusNode.unfocus();
     _focusNode.requestFocus();
     focusInSearch();
+    widget.searchGlossary("");
   }
 
   void focusOutSearch(event) {
