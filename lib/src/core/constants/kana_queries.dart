d8d4b2cff44cc37ebd979d3d2d62e8d873b09c0d
Maxime Guilbert <mx.guilbert@gmail.com>
2024-05-14 10:55:54 -0400

    chore: Old database clean up

--- lib/src/core/constants/kana_queries.dart
+++
@@ -1,6 +0,0 @@
-const _tableName = "kana";
-const getKanaByGroup =
-    "SELECT id, alphabet, group_id, kana, romaji FROM $_tableName WHERE group_id=?";
-
-getKanaByGroups([int listLength = 1]) =>
-    "SELECT id, alphabet, group_id, kana, romaji FROM $_tableName WHERE group_id IN (${List.filled(listLength, "?").join(", ")})";
