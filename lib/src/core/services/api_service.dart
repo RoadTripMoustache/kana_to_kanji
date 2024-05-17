586384c00a5a2b80f43cd8455e622a1bbed2f60e
apomalyn <chretienxavier42@gmail.com>
2024-05-17 18:51:40 -0400

    perf(glossary): Improve performance of the KanaList by switching to GridView.builder

--- lib/src/core/services/api_service.dart
+++ lib/src/core/services/api_service.dart
@@ -3,5 +3,6 @@ import "package:kana_to_kanji/src/core/constants/app_configuration.dart";
 
 class ApiService {
   Future<http.Response> get(String path) =>
-      http.get(Uri.parse("${AppConfiguration.apiUrl}$path"));
+      http.get(Uri.parse("${AppConfiguration.apiUrl}$path"),
+          headers: {"Authorization": "Bearer michel"});
 }
