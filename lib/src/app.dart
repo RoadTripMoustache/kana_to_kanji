f8ad8b84e3594c65681d2680ac4c9f0827598fc7
Maxime G <36586573+Adaendra@users.noreply.github.com>
2024-05-20 22:15:14 -0400

    feat: inform that a new version is available (#255)
    * feat: inform that a new version is available
    
    * fix: Remove unnecessary space
    
    * chore(bot): Bump version from 0.29.0+1 to 0.30.0+1
    
    ---------
    
    Co-authored-by: Xavier Chrétien <chretienxavier42@gmail.com>
    Co-authored-by: Adaendra <Adaendra@users.noreply.github.com>

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,12 +4,15 @@ import "package:firebase_crashlytics/firebase_crashlytics.dart";
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
+
 // ignore: uri_does_not_exist
 import "package:kana_to_kanji/firebase_options.dart";
 import "package:kana_to_kanji/src/core/constants/app_configuration.dart";
 import "package:kana_to_kanji/src/core/constants/app_theme.dart";
 import "package:kana_to_kanji/src/core/repositories/settings_repository.dart";
 import "package:kana_to_kanji/src/core/services/dialog_service.dart";
+import "package:kana_to_kanji/src/core/widgets/app_not_found_view.dart";
+import "package:kana_to_kanji/src/core/widgets/app_snack_bar_upgrader.dart";
 import "package:kana_to_kanji/src/feedback/widgets/feedback_screenshot_form.dart";
 import "package:kana_to_kanji/src/locator.dart";
 import "package:kana_to_kanji/src/router.dart";
@@ -62,21 +65,27 @@ class KanaToKanjiApp extends StatelessWidget {
             feedbackBuilder: (context, onSubmit, _) =>
                 FeedbackScreenshotForm(onSubmit: onSubmit),
             child: MaterialApp.router(
-                restorationScopeId: "app",
+              restorationScopeId: "app",
 
-                // Localizations
-                localizationsDelegates: AppLocalizations.localizationsDelegates,
-                supportedLocales: AppLocalizations.supportedLocales,
-                onGenerateTitle: (BuildContext context) =>
-                    AppLocalizations.of(context).app_title,
-                locale: _settingsRepository.locale,
+              // Localizations
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              onGenerateTitle: (BuildContext context) =>
+                  AppLocalizations.of(context).app_title,
+              locale: _settingsRepository.locale,
 
-                // Theme
-                theme: AppTheme.light(),
-                darkTheme: AppTheme.dark(),
-                themeMode: _settingsRepository.themeMode,
+              // Theme
+              theme: AppTheme.light(),
+              darkTheme: AppTheme.dark(),
+              themeMode: _settingsRepository.themeMode,
 
-                // Router
-                routerConfig: _router),
+              // Router
+              routerConfig: _router,
+              builder: (context, child) => AppSnackBarUpgrader(
+                navigatorKey: _router.routerDelegate.navigatorKey,
+                child:
+                    child ?? AppNotFoundView(uri: Uri(host: ""), goBackUrl: ""),
+              ),
+            ),
           ));
 }
---
+++ lib/src/core/widgets/app_snack_bar_upgrader.dart
@@ -0,0 +1,42 @@
+import "package:flutter/material.dart";
+import "package:flutter_gen/gen_l10n/app_localizations.dart";
+import "package:upgrader/upgrader.dart";
+
+class AppSnackBarUpgrader extends UpgradeAlert {
+  AppSnackBarUpgrader(
+      {super.key, super.navigatorKey, super.upgrader, super.child});
+
+  /// Override the [createState] method to provide a custom class
+  /// with overridden methods.
+  @override
+  UpgradeAlertState createState() => AppSnackBarUpgraderState();
+}
+
+class AppSnackBarUpgraderState extends UpgradeAlertState {
+  /// Show the snack bar to inform the user that a new version
+  /// of the app is available.
+  @override
+  void showTheDialog({
+    required BuildContext context,
+    required String? title,
+    required String message,
+    required String? releaseNotes,
+    required bool barrierDismissible,
+    required UpgraderMessages messages,
+    Key? key,
+  }) {
+    final l10n = AppLocalizations.of(context);
+    final snackBar = SnackBar(
+        content: Text(l10n.snackbar_update_version_message),
+        action: SnackBarAction(
+          label: l10n.snackbar_update_version_action,
+          onPressed: () {
+            onUserUpdated(context, !widget.upgrader.blocked());
+          },
+        ));
+
+    // Find the ScaffoldMessenger in the widget tree
+    // and use it to show a SnackBar.
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
