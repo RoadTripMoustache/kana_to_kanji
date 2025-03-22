8139e22a851d6e21942870aece9aca5e621a77da
apomalyn <22211097+apomalyn@users.noreply.github.com>
2025-03-22 11:49:53 -0400

    chore(deps): bump dependencies
    ref: N/A

--- lib/src/core/models/app_navigation_destination.dart
+++ lib/src/core/models/app_navigation_destination.dart
@@ -4,17 +4,22 @@ import "package:freezed_annotation/freezed_annotation.dart";
 part "app_navigation_destination.freezed.dart";
 
 @freezed
-class AppNavigationDestination with _$AppNavigationDestination {
+sealed class AppNavigationDestination with _$AppNavigationDestination {
   const AppNavigationDestination._();
 
-  const factory AppNavigationDestination(
-      {required Widget icon,
-      required String location,
-      required String label,
-      Widget? selectedIcon,
-      Color? backgroundColor,
-      String? tooltip}) = _AppNavigationDestination;
+  const factory AppNavigationDestination({
+    required Widget icon,
+    required String location,
+    required String label,
+    Widget? selectedIcon,
+    Color? backgroundColor,
+    String? tooltip,
+  }) = _AppNavigationDestination;
 
   NavigationDestination get navigationDestination => NavigationDestination(
-      icon: icon, selectedIcon: selectedIcon, label: label, tooltip: tooltip);
+    icon: icon,
+    selectedIcon: selectedIcon,
+    label: label,
+    tooltip: tooltip,
+  );
 }
