6dd04795827bb59f59bb050ec5569d5436845e2e
apomalyn <22211097+apomalyn@users.noreply.github.com>
2025-04-28 12:13:26 -0400

    feat(sync): implement PaginatedResponse model
    ref: #370

---
+++ lib/src/core/models/paginated_response.dart
@@ -0,0 +1,39 @@
+import "package:freezed_annotation/freezed_annotation.dart";
+import "package:kana_to_kanji/src/core/models/resource.dart";
+
+part "paginated_response.freezed.dart";
+
+part "paginated_response.g.dart";
+
+@Freezed(toJson: false, genericArgumentFactories: true)
+sealed class PaginatedResponse<T extends Resource> with _$PaginatedResponse<T> {
+  const PaginatedResponse._();
+
+  const factory PaginatedResponse({
+    required PageLinks links,
+    required List<T> data,
+  }) = _PaginatedResponse;
+
+  factory PaginatedResponse.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object?) fromJsonT,
+  ) => _$PaginatedResponseFromJson(json, fromJsonT);
+
+  bool get hasMore => links.hasMore;
+}
+
+@Freezed(toJson: false)
+sealed class PageLinks with _$PageLinks {
+  const factory PageLinks({
+    required String first,
+    required String previous,
+    required String next,
+    required String self,
+    required String prev,
+    required String last,
+    required bool hasMore,
+  }) = _PageLinks;
+
+  factory PageLinks.fromJson(Map<String, dynamic> json) =>
+      _$PageLinksFromJson(json);
+}
