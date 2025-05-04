b89443a8888f304b43a418c499720f999220c310
apomalyn <22211097+apomalyn@users.noreply.github.com>
2025-05-04 02:33:12 -0400

    feat(glossary): add no result widget for Kanji and vocabulary
    closes #101

--- lib/src/glossary/widgets/kanji_list.dart
+++ lib/src/glossary/widgets/kanji_list.dart
@@ -1,9 +1,11 @@
 import "package:flutter/material.dart";
 import "package:flutter_rtm/flutter_rtm.dart";
 import "package:infinite_scroll_pagination/infinite_scroll_pagination.dart";
+import "package:kana_to_kanji/src/core/constants/resource_type.dart";
 import "package:kana_to_kanji/src/core/models/resources/kanji.dart";
 import "package:kana_to_kanji/src/glossary/pagination_helper.dart";
 import "package:kana_to_kanji/src/glossary/widgets/glossary_list_tile.dart";
+import "package:kana_to_kanji/src/glossary/widgets/search_no_result.dart";
 
 class KanjiList extends StatelessWidget {
   final Function(Kanji)? onPressed;
@@ -21,6 +23,8 @@ class KanjiList extends StatelessWidget {
       firstPageProgressIndicatorBuilder: (context) => const Center(),
       newPageProgressIndicatorBuilder:
           (context) => const Center(child: RTMSpinner()),
+      noItemsFoundIndicatorBuilder:
+          (context) => const SearchNoResult(type: ResourceType.kanji),
       animateTransitions: true,
       transitionDuration: const Duration(milliseconds: 400),
       itemBuilder:
--- lib/src/glossary/widgets/vocabulary_list.dart
+++ lib/src/glossary/widgets/vocabulary_list.dart
@@ -1,9 +1,11 @@
 import "package:flutter/material.dart";
 import "package:flutter_rtm/flutter_rtm.dart";
 import "package:infinite_scroll_pagination/infinite_scroll_pagination.dart";
+import "package:kana_to_kanji/src/core/constants/resource_type.dart";
 import "package:kana_to_kanji/src/core/models/resources/vocabulary.dart";
 import "package:kana_to_kanji/src/glossary/pagination_helper.dart";
 import "package:kana_to_kanji/src/glossary/widgets/glossary_list_tile.dart";
+import "package:kana_to_kanji/src/glossary/widgets/search_no_result.dart";
 
 class VocabularyList extends StatelessWidget {
   final Function(Vocabulary)? onPressed;
@@ -21,6 +23,8 @@ class VocabularyList extends StatelessWidget {
       firstPageProgressIndicatorBuilder: (context) => const Center(),
       newPageProgressIndicatorBuilder:
           (context) => const Center(child: RTMSpinner()),
+      noItemsFoundIndicatorBuilder:
+          (context) => const SearchNoResult(type: ResourceType.vocabulary),
       animateTransitions: true,
       transitionDuration: const Duration(milliseconds: 400),
       itemBuilder:
